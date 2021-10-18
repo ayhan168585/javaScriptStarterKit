95c7c86121ceb2b84a87ec8440547e0d84b30d9b
ayhan168585 <49242925+ayhan168585@users.noreply.github.com>
2021-10-19 00:00:47 +0300

    19.10.2021-00:00

---
+++ src/app.js
@@ -0,0 +1,15 @@
+console.log("Merhaba Kodlama.io");
+
+let dolarBugun = 9.3;
+
+let dolarDun = 9.2;
+console.log(dolarDun);
+
+const euroDun = 11.2;
+//euroDun=11
+
+console.log(euroDun);
+
+let konutKredileri=["Konut kredisi","Emlak konut kredisi","Kamu konut kredisi"]
+
+console.log(konutKredileri)
