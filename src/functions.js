c7cb0942cf3d88315146072f2e978608c38a8c08
ayhan168585 <49242925+ayhan168585@users.noreply.github.com>
2021-11-01 22:01:45 +0300

    01.11.2021-22:00

---
+++ src/functions.js
@@ -0,0 +1,21 @@
+function addToCart(quantity,productName="Elma") {
+    
+    console.log("Sepete eklendi." + productName +" "+ "adet:"+quantity)
+}
+
+//addToCart()
+addToCart(10)
+//addToCart("Karpuz")
+
+
+let sayHello=()=>{
+    console.log("Hello")
+}
+
+sayHello()
+
+let sayHello2=function () {
+    console.log("Hello2")
+}
+
+sayHello2()
